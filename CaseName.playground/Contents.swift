4dfc8a8fb73d2e15187691b514c088314b61410a
Moslienko <moslienko@icloud.com>
2018-09-19 19:45:00 +0700

    Добавлены новые правила
    Добавлены исключения из правил.
    Падеж выбирается после получения значения из функции, возвращается структура.
    Исправлена проверка окончаний типа "ья" и т.д.
    Вспомогательная функция - есть ли шипиящие буквы в имени.
    Улучшен пример работы.

--- CaseName.playground/Contents.swift
+++ CaseName.playground/Contents.swift
@@ -9,6 +9,14 @@ import Foundation
 
 extension String {
     
+    struct cases {
+        var genitive:String //р.п
+        var dative:String //в.п
+        var accusative:String //д.р
+        var instrumental:String //т.п
+        var prepositional:String //п.п
+    }
+    
     /**
      Определение, является ли буква согласной
      - parameter name: Буква
@@ -25,21 +33,29 @@ extension String {
     
     /**
      Склонение имени
-     - parameter caseVal: Имя
      */
-    func caseName(caseVal:String) -> String {
+    func caseName() -> cases {
         let name = self
+        let nameSuffix = name.suffix(1) //Окончание имени
+        //Последняя буква - согласная
+        let isConsonant = isConsonantLetter(name: nameSuffix)
         
-        if name.suffix(1) == "ь" {
-           return optionOne(name: name, caseVal: caseVal)
+        if nameSuffix == "ь" {
+           return optionOne(name: name)
         }
         
-        let isConsonant = isConsonantLetter(name: name.suffix(1))
-        
-        if name.suffix(1) == "й" || isConsonant  {
-            return optionTwo(name: name, caseVal: caseVal, consonant: isConsonant)
+        if nameSuffix == "й" || isConsonant  {
+            return optionTwo(name: name, consonant: isConsonant)
         }
-        return name
+        
+        if nameSuffix == "а" {
+            return optionThree(name: name)
+        }
+        if nameSuffix == "я" || name.suffix(2) == "ья" || name.suffix(2) == "ия" || name.suffix(2) == "ея" {
+            return optionFour(name: name)
+        }
+        
+        return cases.init(genitive:name, dative:name, accusative:name, instrumental:name, prepositional:name)
     }
     
     /**
@@ -55,69 +71,210 @@ extension String {
     }
     
     /**
-     Склонение по первому варианту - женские имена в 3-м склонении (оканчивающиеся на -ь)
-     - parameters:
-        - name: Имя
-        - caseVal: Падеж
+     Является ли имя мужским, применяется только для склонения по первому варианту
+     - parameter name: Имя
      */
-    func optionOne(name:String,caseVal:String) -> String {
-        switch caseVal {
-            case "i": return name
-            case "r": return String(name.dropLast()) + "и"
-            case "d": return String(name.dropLast()) + "и"
-            case "w": return name
-            case "t": return String(name.dropLast()) + "ью"
-            case "p": return getAboutTitle(firstLeter: name.prefix(1)) +  String(name.dropLast()) + "и"
-
-            default: return name
+    func isManName(name value:String) -> Bool {
+        let manNames = ["Игорь", "Израиль", "Камиль", "Лазарь", "Марсель", "Наиль", "Олесь", "Равиль", "Рамиль", "Фидель", "Цезарь", "Эмиль", "Виль", "Рауль", "Шамиль", "Петрусь"]
+        
+        for name in manNames {
+            if name.lowercased() == value.lowercased() {
+                return true
+            }
         }
+        return false
+    }
+    
+    /**
+     Склонение по первому варианту - имена в 3-м склонении (оканчивающиеся на -ь)
+     - parameter caseVal: Падеж
+     */
+    func optionOne(name:String) -> cases {
+        //Разные правила для мужских и женских имен, оканчивающихся на мягкие согласные
+        if isManName(name: name) {
+            
+            return cases.init(
+                genitive: String(name.dropLast()) + "я",
+                dative: String(name.dropLast()) + "ю",
+                accusative: String(name.dropLast()) + "я",
+                instrumental: String(name.dropLast()) + "ем",
+                prepositional: getAboutTitle(firstLeter: name.prefix(1)) +  String(name.dropLast()) + "е"
+            )
+
+        }
+        else {
+            
+            return cases.init(
+                genitive: String(name.dropLast()) + "и",
+                dative: String(name.dropLast()) + "и",
+                accusative: name,
+                instrumental: String(name.dropLast()) + "ью",
+                prepositional: getAboutTitle(firstLeter: name.prefix(1)) +  String(name.dropLast()) + "и"
+            )
+            
+        }
+    }
+    
+    /**
+     Исправляет имя для второго варианта, если в нем должны появлятся беглые гласные
+     - parameter name: Имя
+     */
+    func exceptionForOptionTwo(name:String) -> String {
+        if name == "Лев" {
+            return "Льв"
+        }
+        if name == "Павел" {
+            return "Павл"
+        }
+        return name
+    }
+    
+    /**
+     Присутствуют ли шипящие в имени
+     - parameter letter: Имя
+     */
+    func isSizzlingLetter(letter value:String.SubSequence) -> Bool {
+        let sizzlingLetters = ["ш","ж","ч","щ","ц"]
+        for letter in sizzlingLetters {
+            if letter == value.lowercased() {
+                return true
+            }
+        }
+        return false
     }
     
     /**
      Склонение по второму варианту - мужские имена, оканчивающиеся на согласный и на –й:
      - parameters:
-     - name: Имя
-     - caseVal: Падеж
+        - name: Имя
+        - consonant: Оканчивается на согласную
      */
-    func optionTwo(name:String,caseVal:String,consonant:Bool) -> String {
+    func optionTwo(name:String,consonant:Bool) -> cases {
         if consonant {
-            switch caseVal {
-            case "i": return name
-            case "r": return name + "а"
-            case "d": return name + "у"
-            case "w": return name + "а"
-            case "t": return name + "ом"
-            case "p": return getAboutTitle(firstLeter: name.prefix(1)) + name + "е"
-                
-            default: return name
-            }
+            let correctName = exceptionForOptionTwo(name: name) //Для имен - исключений из правил
+            //Исправляет окончание в творительном падеже, если в имени есть шипящие или "ц"
+            let endingWordForT = isSizzlingLetter(letter: name.suffix(1)) == true ? "ем" : "ом"
+            
+            return cases.init(
+                genitive: correctName + "а",
+                dative: correctName + "а",
+                accusative: correctName + "у",
+                instrumental: correctName + endingWordForT,
+                prepositional: getAboutTitle(firstLeter: name.prefix(1)) + correctName + "е"
+            )
+
         }
         else {
-            switch caseVal {
-            case "i": return name
-            case "r": return String(name.dropLast()) + "я"
-            case "d": return String(name.dropLast()) + "ю"
-            case "w": return String(name.dropLast()) + "я"
-            case "t": return String(name.dropLast()) + "ем"
-            case "p": return getAboutTitle(firstLeter: name.prefix(1)) + String(name.dropLast()) + "е"
-                
-            default: return name
-            }
+            
+            return cases.init(
+                genitive: String(name.dropLast()) + "я",
+                dative: String(name.dropLast()) + "ю",
+                accusative: String(name.dropLast()) + "я",
+                instrumental: String(name.dropLast()) + "ем",
+                prepositional: getAboutTitle(firstLeter: name.prefix(1)) + String(name.dropLast()) + "е"
+            )
+            
         }
+    }
+    
+    
+    /**
+     Склонение по третьему варианту - мужские и женские имена, оканчивающиеся на –а:
+     - parameter caseVal: Падеж
+     */
+    func optionThree(name:String) -> cases {
+        
+        return cases.init(
+            genitive: String(name.dropLast()) + "ы",
+            dative: String(name.dropLast()) + "е",
+            accusative: String(name.dropLast()) + "у",
+            instrumental: String(name.dropLast()) + "ой",
+            prepositional:getAboutTitle(firstLeter: name.prefix(1)) + String(name.dropLast()) + "е"
+        )
+
+    }
+    
+    /**
+     Получение имени в падежах для четвертого варианта в зависимости от окончания слова
+     - parameter name: Имя
+     */
+    func endingName(name:String) -> cases {
+        let endingName = name.suffix(2)
+        let endingNameOneLetter = name.suffix(1)
+
+        if endingName == "ия" {
+            //род.,дат.,пр.падежи - одинаково
+            let genitive = String(name.dropLast(2)) + "ии"
+            let accusative = genitive
+            let prepositional = genitive
+            
+            return cases.init(genitive:genitive,dative:"", accusative:accusative,instrumental:"",prepositional:prepositional)
+        }
+        if endingName == "ья" {
+            let genitive = String(name.dropLast(1)) + "и"
+            let accusative = String(name.dropLast(1)) + "e"
+            let prepositional = String(name.dropLast(1)) + "е"
+            
+            return cases.init(genitive:genitive,dative:"", accusative:accusative,instrumental:"",prepositional:prepositional)
+        }
+        
+        if endingNameOneLetter == "я", endingName != "ья" {
+            let genitive = String(name.dropLast(1)) + "и"
+            let accusative = String(name.dropLast(1)) + "е"
+            let dative = String(name.dropLast(1)) + "ю"
+            let instrumental = String(name.dropLast(1)) + "ей"
+            let prepositional = String(name.dropLast(1)) + "е"
+            
+            return cases.init(genitive:genitive,dative:dative, accusative:accusative,instrumental:instrumental,prepositional:prepositional)
+        }
+        //По умолчанию
+            let genitive = String(name.dropLast()) + "е"
+            let accusative = String(name.dropLast()) + "е"
+            let prepositional = String(name.dropLast()) + "е"
+            
+            return cases.init(genitive:genitive,dative:"", accusative:accusative,instrumental:"",prepositional:prepositional)
+    }
+    
+    /**
+     Склонение по четвертому варианту - мужские и женские имена, оканчивающиеся иа -я, -ья, -ия, -ея:
+     - parameter name: Имя
+     */
+    func optionFour(name:String) -> cases {
+        let caseWithEndName = endingName(name: name)
+
+        return cases.init(
+            genitive: caseWithEndName.genitive,
+            dative: caseWithEndName.dative != "" ? caseWithEndName.dative : String(name.dropLast()) + "ю",
+            accusative: String(name.dropLast()) + "е",
+            instrumental: caseWithEndName.instrumental != "" ? caseWithEndName.instrumental : String(name.dropLast()) + "ей",
+            prepositional:getAboutTitle(firstLeter: name.prefix(1)) + caseWithEndName.prepositional
+        )
+
     }
     
 }
 
-func demo() {
-    let names = ["Иван", "Жизель", "Александр", "Любовь", "Павел", "Алексей", "Михаил","Элла"]
-    let cases = ["r", "d", "w", "t", "p"]
+let names1 = ["Иван", "Жизель", "Александр", "Любовь", "Павел", "Алексей", "Михаил","Элла", "Алиса", "Илья", "Наталья", "Мария"]
+let names2 = ["Иван", "Святослав", "Тимур", "Рем"]
+let names3 = ["Петр", "Лев", "Павел", "Фрол"]
+let names4 = ["Юмаш", "Жорж", "Ференц", "Казбич"]
+let names5 = ["Игорь", "Любовь", "Виль", "Рауль", "Шамиль", "Петрусь"]
+let names6 = ["Никита", "Данила", "Кузьма", "Мустафа"]
+let names7 = ["Олеся", "Дарья", "Алиса"]
+
+func demo(names:[String]) {
 
     for name in names {
-        for caseVal in cases {
-            print (name.caseName(caseVal: caseVal))
-        }
+        print ("И.П: ",name)
+        print ("Р.П: ",name.caseName().genitive)
+        print ("Д.П: ",name.caseName().accusative)
+        print ("В.П: ",name.caseName().dative)
+        print ("Т.П: ",name.caseName().instrumental)
+        print ("П.П: ",name.caseName().prepositional)
+        
+        print ("\n")
+
     }
 }
-demo()
 
-
+demo(names: names7)
