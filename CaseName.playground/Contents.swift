6700aef80be231023a91fa361998ac378870019a
Moslienko <moslienko@icloud.com>
2018-09-20 14:05:09 +0700

    Склонение отчества
    Исправлена ошибка д. и в. падежей в некоторых алгоритмах.

--- CaseName.playground/Contents.swift
+++ CaseName.playground/Contents.swift
@@ -11,8 +11,8 @@ extension String {
     
     struct cases {
         var genitive:String //р.п
-        var dative:String //в.п
-        var accusative:String //д.р
+        var dative:String //д.п
+        var accusative:String //в.п
         var instrumental:String //т.п
         var prepositional:String //п.п
     }
@@ -51,11 +51,41 @@ extension String {
         if nameSuffix == "а" {
             return optionThree(name: name)
         }
-        if nameSuffix == "я" || name.suffix(2) == "ья" || name.suffix(2) == "ия" || name.suffix(2) == "ея" {
+        if nameSuffix == "я" {
             return optionFour(name: name)
         }
         
         return cases.init(genitive:name, dative:name, accusative:name, instrumental:name, prepositional:name)
+    }
+    
+    /**
+     Склонение отчества
+     */
+    func caseMiddleName() -> cases {
+        let middleNameSuffix = self.suffix(1) //Окончание отчества
+        let newMiddleName = String(self.dropLast(1))
+        
+        if self.suffix(2) == "ич" {
+            return cases.init(
+                genitive: self + "а",
+                dative: self + "у",
+                accusative: self + "a",
+                instrumental: self + "ем",
+                prepositional: self + "е"
+            )
+        }
+        
+        if self.suffix(2) == "на" {
+            return cases.init(
+                genitive: newMiddleName + "ы",
+                dative: newMiddleName + "е",
+                accusative: newMiddleName + "у",
+                instrumental: newMiddleName + "ой",
+                prepositional: newMiddleName + "е"
+            )
+            
+        }
+        return cases.init(genitive: self, dative: self, accusative: self, instrumental: self, prepositional: self)
     }
     
     /**
@@ -157,8 +187,8 @@ extension String {
             
             return cases.init(
                 genitive: correctName + "а",
-                dative: correctName + "а",
-                accusative: correctName + "у",
+                dative: correctName + "у",
+                accusative: correctName + "а",
                 instrumental: correctName + endingWordForT,
                 prepositional: getAboutTitle(firstLeter: name.prefix(1)) + correctName + "е"
             )
@@ -220,8 +250,8 @@ extension String {
         
         if endingNameOneLetter == "я", endingName != "ья" {
             let genitive = String(name.dropLast(1)) + "и"
-            let accusative = String(name.dropLast(1)) + "е"
-            let dative = String(name.dropLast(1)) + "ю"
+            let accusative = String(name.dropLast(1)) + "ю"
+            let dative = String(name.dropLast(1)) + "е"
             let instrumental = String(name.dropLast(1)) + "ей"
             let prepositional = String(name.dropLast(1)) + "е"
             
@@ -241,11 +271,10 @@ extension String {
      */
     func optionFour(name:String) -> cases {
         let caseWithEndName = endingName(name: name)
-
         return cases.init(
             genitive: caseWithEndName.genitive,
-            dative: caseWithEndName.dative != "" ? caseWithEndName.dative : String(name.dropLast()) + "ю",
-            accusative: String(name.dropLast()) + "е",
+            dative: caseWithEndName.dative != "" ? caseWithEndName.dative : String(name.dropLast()) + "е",
+            accusative: String(name.dropLast()) + "ю",
             instrumental: caseWithEndName.instrumental != "" ? caseWithEndName.instrumental : String(name.dropLast()) + "ей",
             prepositional:getAboutTitle(firstLeter: name.prefix(1)) + caseWithEndName.prepositional
         )
@@ -260,15 +289,18 @@ let names3 = ["Петр", "Лев", "Павел", "Фрол"]
 let names4 = ["Юмаш", "Жорж", "Ференц", "Казбич"]
 let names5 = ["Игорь", "Любовь", "Виль", "Рауль", "Шамиль", "Петрусь"]
 let names6 = ["Никита", "Данила", "Кузьма", "Мустафа"]
-let names7 = ["Олеся", "Дарья", "Алиса"]
+let names7 = ["Олеся", "Дарья", "Илья"]
 
-func demo(names:[String]) {
+let middleName1 = ["Евгеньевич", "Владимировна", "Никитич"]
+
+
+func demoName(names:[String]) {
 
     for name in names {
         print ("И.П: ",name)
         print ("Р.П: ",name.caseName().genitive)
-        print ("Д.П: ",name.caseName().accusative)
-        print ("В.П: ",name.caseName().dative)
+        print ("Д.П: ",name.caseName().dative)
+        print ("В.П: ",name.caseName().accusative)
         print ("Т.П: ",name.caseName().instrumental)
         print ("П.П: ",name.caseName().prepositional)
         
@@ -277,4 +309,20 @@ func demo(names:[String]) {
     }
 }
 
-demo(names: names7)
+func demoMiddleName(names:[String]) {
+    
+    for name in names {
+        print ("И.П: ",name)
+        print ("Р.П: ",name.caseMiddleName().genitive)
+        print ("Д.П: ",name.caseMiddleName().dative)
+        print ("В.П: ",name.caseMiddleName().accusative)
+        print ("Т.П: ",name.caseMiddleName().instrumental)
+        print ("П.П: ",name.caseMiddleName().prepositional)
+        
+        print ("\n")
+        
+    }
+}
+
+demoName(names: names1)
+demoMiddleName(names: middleName1)
